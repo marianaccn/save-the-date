20726e302ba27deef54013dd9675e1a9a17983d4
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2021-10-21 22:01:18 -0300

    add ionic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Save the date imports */
+
 import { GlobalStyle } from './styles/global';
-import { Routes } from './routes';
+// import { Routes } from './routes';
+import '@ionic/react/css/core.css';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { Route } from 'react-router';
+import { HomePage } from './components/Pages/HomePage';
+import { LandingPage } from './components/Pages/LandingPage';
+import { LoginPage } from './components/Pages/LoginPage';
+import { ProfilePage } from './components/Pages/ProfilePage';
+import { RegistrationPage } from './components/Pages/RegistrationPage';
 
 export function App() {
   return (
-    <>
-      <Routes />
+    <IonApp>
+      <IonReactRouter>
+        <IonRouterOutlet>
+          <Route exact path="/registrationPage">
+            <RegistrationPage />
+          </Route>
+          <Route exact path="/profilePage">
+            <ProfilePage />
+          </Route>
+          <Route exact path="/homePage">
+            <HomePage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/landingPage">
+            <LandingPage />
+          </Route>
+        </IonRouterOutlet>
+      </IonReactRouter>
       <GlobalStyle />
-    </>
+    </IonApp>
   );
 }
